23f36463cd290678732da66eb9725250df402ca2
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-19 15:19:43 +0100

    Add button events

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Getting Elements
+const addBtn = document.querySelector('.add-btn');
+const removeBtn = document.querySelector('.remove-btn');
+
 const books = [
-    {
-     name:'',
-     title: '',
-     Author: ''
-    }
-]
+  {
+    name: '',
+    title: '',
+    Author: '',
+  },
+];
