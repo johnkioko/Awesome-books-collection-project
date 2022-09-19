e34619fdd3815b2dcee5f1fbfbb0037e1f6d0b22
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-19 16:54:14 +0100

    Add local storage function

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 // Getting Elements
-const addBtn = document.querySelector('.add-btn');
-const removeBtn = document.querySelector('.remove-btn');
+// const addBtn = document.querySelector('.add-btn');
+// const removeBtn = document.querySelector('.remove-btn');
 
-const books = [
-  {
-    name: '',
-    title: '',
-    Author: '',
-  },
-];
+// const books = [
+//   {
+//     name: '',
+//     title: '',
+//     Author: '',
+//   },
+// ];
 
+// function addBook() {
+//   addBtn.addEventListener('click', () => {});
+// }
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const form = document.getElementById('book-form');
+const library = document.getElementById('library');
+let book;
+let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 function addBook() {
-  addBtn.addEventListener('click', () => {});
+  book = {
+    title: title.value,
+    author: author.value,
+    id: Math.floor(Math.random() * 1000000),
+  };
+  bookList.push(book);
+  localStorage.setItem('bookList', JSON.stringify(bookList));
 }
+function removeBook(id) {
+  bookList = bookList.filter((books) => books.id !== id);
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+function populate(book) {
+  const row = document.createElement('tr');
+  const bookTitle = document.createElement('td');
+  const bookAuthor = document.createElement('td');
+  const removeBtn = document.createElement('button');
+  bookTitle.innerText = book.title;
+  bookAuthor.innerText = book.author;
+  removeBtn.innerText = 'Remove';
+  row.append(bookTitle, bookAuthor, removeBtn);
+  library.append(row);
+  removeBtn.addEventListener('click', () => {
+    removeBtn.parentElement.remove();
+    removeBook(book.id);
+  });
+}
+bookList.forEach(populate);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (title.value !== '' && author.value !== '') {
+    addBook();
+    populate(book);
+    form.reset();
+  } else {
+    alert('Please enter a title and author');
+  }
+});
