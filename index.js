dffc01082225889e4936a0f0e55a9766ae850672
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-20 18:12:44 +0100

    Add object

--- index.js
+++ index.js
@@ -1,50 +1,90 @@
-// Getting Elements
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const form = document.getElementById('book-form');
-const library = document.getElementById('library');
-let book;
-let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
-
-function addBook() {
-  book = {
-    title: title.value,
-    author: author.value,
-    id: Math.floor(Math.random() * 1000000),
-  };
-  bookList.push(book);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
-
-function removeBook(id) {
-  bookList = bookList.filter((books) => books.id !== id);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
+  static addBookToList(book) {
+    const books = document.querySelector('.books');
+    const newBook = document.createElement('div');
+    newBook.innerHTML = `
+      <div>
+      <div class="bookDiv">
+      <div class="bookTitleAuthor">
+      <h4 id="title">"${book.title}"</h4>
+      <p class="by">by</p>
+      <h4 id="author" >${book.author}</h4>
+      </div>
+      <button class="delete">Remove</button>
+      </div>
+      <hr class="hr">
+      </div>
+      `;
+    newBook.classList.add('newBook');
+    books.appendChild(newBook);
+  }
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+  static clearFields() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+  }
 }
-function populate(book) {
-  const row = document.createElement('tr');
-  const bookTitle = document.createElement('td');
-  const bookAuthor = document.createElement('td');
-  const removeBtn = document.createElement('button');
-  bookTitle.innerText = book.title;
-  bookAuthor.innerText = book.author;
-  removeBtn.innerText = 'Remove';
-  row.append(bookTitle, bookAuthor, removeBtn);
-  library.append(row);
-  removeBtn.addEventListener('click', () => {
-    removeBtn.parentElement.remove();
-    removeBook(book.id);
-  });
+// STORE CLASS
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(book) {
+    const bookTitle = book.querySelector('#title').innerText;
+    const books = Store.getBooks();
+    const filt = books.filter((book) => bookTitle === book.title);
+    const filtIndex = books.indexOf(filt[0]);
+    books.splice(filtIndex, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
-
-bookList.forEach(populate);
-
-form.addEventListener('submit', (e) => {
+//EVENT TO DISPLAY BOOKS
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+//EVENT TO ADD A BOOK
+document.querySelector('.bookForm').addEventListener('submit', (e) => {
   e.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    addBook();
-    populate(book);
-    form.reset();
-  } else {
-    alert('Please enter a title and author');
+  //get form values
+  const titleInput = document.querySelector('.title').value;
+  const authorInput = document.querySelector('.author').value;
+  const book = new Book(titleInput, authorInput);
+  console.log(book);
+  //ADD BOOK TO LIST
+  UI.addBookToList(book);
+  //ADD BOOK TO STORE
+  Store.addBook(book);
+  //CLEAR FIELDS
+  UI.clearFields();
+});
+//EVENT DELETE
+document.querySelector('.books').addEventListener('click', (e) => {
+  if (e.target.className === 'delete') {
+    const book = e.target.parentElement;
+    Store.removeBook(book);
+    UI.deleteBook(e.target);
   }
 });
